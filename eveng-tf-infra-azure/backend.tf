27b8be07ac437c1cdaa21aa2897e5168ea13caf0
pizour <z.pyszko@gmail.com>
2024-11-27 22:55:17 +0100

    storage account ready

--- eveng-tf-infra-azure/backend.tf
+++ eveng-tf-infra-azure/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "terraformstate"
+    storage_account_name = "evengtfstate"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
