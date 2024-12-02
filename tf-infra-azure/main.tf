d854a4c665e399de0ba8330a5d93de77a646cd12
pizour <z.pyszko@gmail.com>
2024-12-02 13:47:29 +0100

    address_space -> net

--- tf-infra-azure/main.tf
+++ tf-infra-azure/main.tf
@@ -10,7 +10,7 @@ module "networking" {
   for_each = { for vnet in local.iac["infra"]["networks"] : vnet.name => vnet }
 
   vnet_name           = each.value.name
-  address_space       = each.value.address_space
+  address_space       = each.value.net
   subnets             = each.value.subnets
   resource_group_name = local.iac["infra"]["rg"]
   location            = local.iac["infra"]["azure_location"]
