5c79980e066b3d2d8f21424b424555f485e75ab8
pizour <z.pyszko@gmail.com>
2024-12-02 14:38:26 +0100

    nsg fix

--- tf-infra-azure/modules/tf-mod-compute/main.tf
+++ tf-infra-azure/modules/tf-mod-compute/main.tf
@@ -6,9 +6,9 @@ resource "azurerm_public_ip" "public_ip" {
 }
 
 resource "azurerm_network_security_group" "network_security_group" {
-  name                = "myNetworkSecurityGroup"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  name                = "${var.vm_name}-nsg"
+  resource_group_name = var.resource_group_name
+  location            = var.location
 
   security_rule {
     name                       = "SSH"
