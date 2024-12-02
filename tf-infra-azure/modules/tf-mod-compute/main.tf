e1bad316782ddefe8bafd6966d01bb2711d94238
pizour <z.pyszko@gmail.com>
2024-12-02 15:41:34 +0100

    manual approval

--- tf-infra-azure/modules/tf-mod-compute/main.tf
+++ tf-infra-azure/modules/tf-mod-compute/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "${var.vm_name}-pip"
   resource_group_name = var.resource_group_name
   location            = var.location
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   #domain_name_label   = "example-vm" # This will be the FQDN, example-vm.<region>.cloudapp.azure.com
 }
 
