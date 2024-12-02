91a158b451f53a7a102e139091a553e7928162c3
pizour <z.pyszko@gmail.com>
2024-12-02 13:36:10 +0100

    for_each = subnet in var.subnets

--- tf-infra-azure/modules/tf-mod-networking/main.tf
+++ tf-infra-azure/modules/tf-mod-networking/main.tf
@@ -6,12 +6,12 @@ resource "azurerm_virtual_network" "virtual_network" {
 }
 
 resource "azurerm_subnet" "subnet" {
-  for_each = var.subnets
+  for_each = subnet in var.subnets
 
-  name                 = each.value.name
+  name                 = subnet.name
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.virtual_network.name
-  address_prefixes     = [each.value.net]
+  address_prefixes     = [subnet.net]
 }
 
 # resource "azurerm_public_ip" "eveng-pip" {
