699296a87434168076b3a5f856456d71ff491d77
pizour <z.pyszko@gmail.com>
2024-12-02 13:48:48 +0100

    rg and location opposite

--- tf-infra-azure/modules/tf-mod-networking/main.tf
+++ tf-infra-azure/modules/tf-mod-networking/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_virtual_network" "virtual_network" {
   name                = var.vnet_name
   address_space       = [var.address_space]
-  location            = var.resource_group_name
-  resource_group_name = var.location
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_subnet" "subnet" {
