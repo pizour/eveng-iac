59b57362702587879653cd5f97d64532431068b4
pizour <z.pyszko@gmail.com>
2024-12-02 14:53:57 +0100

    pubkey

--- tf-infra-azure/modules/tf-mod-networking/outputs.tf
+++
@@ -1,6 +0,0 @@
-# output "virtual_network_subnets" {
-#   description = "Maps out virtual network subnets created with names and ids"
-#   value = tomap({
-#     for k, subnet in azurerm_subnet.subnet : k => { id : subnet.id, name : subnet.name, address_prefixes : subnet.address_prefixes }
-#   })
-# }
