5888ed52b32ab2d296cca3051b863ca8651a2008
pizour <z.pyszko@gmail.com>
2024-12-03 14:49:39 +0100

    ansible-test

---
+++ ans-app-eveng/inventory/dynamic_iac_inventory.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import yaml
+import json
+
+def load_yaml_inventory():
+    # Open the YAML file that contains your inventory
+    with open('../../tf_infra_iac.yml', 'r') as file:
+        inventory_data = yaml.safe_load(file)
+    
+    return inventory_data
+
+def main():
+    # Load inventory from YAML
+    inventory = load_yaml_inventory()
+
+    # Generate hosts and hostvars
+    hosts = {}
+    for vm in inventory.get("infra", {}).get("vms", []):
+        alias = f'{vm.get("unique_vm_alias")}.{inventory["infra"]["azure_location"]}.cloudapp.azure.com'
+        hosts[alias] = {
+            'vm_name': vm.get("vm_name"),
+            'ssh_username': vm.get("ssh_username"),
+            'ssh_pubkey': vm.get("ssh_pubkey")
+        }
+
+    output = {
+        'all': {
+            'hosts': list(hosts.keys())
+        },
+        '_meta': {
+            'hostvars': hosts
+        }
+    }
+
+    # Print the JSON-formatted output
+    print(json.dumps(output, indent=2))
+
+if __name__ == "__main__":
+    main()
