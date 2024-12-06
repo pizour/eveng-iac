53afef5991c71a1130ce2d1b1c2c23de98d2042c
pizour <z.pyszko@gmail.com>
2024-12-06 09:05:42 +0100

    deploy eve

--- ans-app-eveng/plugins/modules/module_utils/eveng.py
+++ ans-app-eveng/plugins/modules/module_utils/eveng.py
@@ -1,9 +1,7 @@
 from evengsdk.client import EvengClient
 
 def eveng_connect(host, username, password):
-    conn = EvengClient(host=host)
+    conn = EvengClient(host=host, ssl_verify=False, protocol="https")
+    conn.disable_insecure_warnings()
     conn.login(username=username, password=password)
     return conn
-
-def change_admin_password(conn, new_password):
-    return conn.api.auth_change_password(new_password)
