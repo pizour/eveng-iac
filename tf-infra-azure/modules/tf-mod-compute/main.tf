1dc48b65192a4db593ba7ee2854044b94bf65d08
pizour <z.pyszko@gmail.com>
2025-02-27 13:29:30 +0100

    infra up

--- tf-infra-azure/modules/tf-mod-compute/main.tf
+++ tf-infra-azure/modules/tf-mod-compute/main.tf
@@ -34,6 +34,18 @@ resource "azurerm_network_security_group" "network_security_group" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1020
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface" "network_interface" {
